b229d281516950748ed2810d6ba06d866ab52818
Wilson lo <wilsonlo1997@gmail.com>
2019-01-18 12:26:10 +0800

    update unit testing

--- unittest.py
+++ unittest.py
@@ -6,7 +6,7 @@ import requests
 def _():
     @it('routing:get_player_card_data')
     def test1():
-        url = "http://0.0.0.0:8080/get_player_card_data"
+        url = "localhost:8080/get_player_card_data"
         payload = ""
         headers = {
             'Content-Type': "application/json",
@@ -18,7 +18,7 @@ def _():
 
     @it('routing:get_mission_card_data')
     def test1():
-        url = "http://0.0.0.0:8080/get_mission_card_data"
+        url = "localhost:8080/get_mission_card_data"
         payload = ""
         headers = {
             'Content-Type': "application/json",
