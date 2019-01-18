bdf9a07c1c5d7cfd5bf25ac271b808c0c9236099
Wilson lo <wilsonlo1997@gmail.com>
2019-01-18 14:13:57 +0800

    update unit testing

--- unittest.py
+++ unittest.py
@@ -4,9 +4,21 @@ import requests
 
 @describe('routing testing')
 def _():
+    @it('routing:api system check')
+    def test_init():
+        url = "http://127.0.0.1:8080/"
+        payload = ""
+        headers = {
+            'Content-Type': "application/json",
+            'cache-control': "no-cache",
+            'Postman-Token': "7759277d-6293-49e5-b194-5b0a2085b1e1"
+        }
+        response = requests.request("POST", url, data=payload, headers=headers)
+        # assert (response.text == "...")
+
     @it('routing:get_player_card_data')
     def test1():
-        url = "http://localhost:8080/get_player_card_data"
+        url = "http://127.0.0.1:8080/get_player_card_data"
         payload = ""
         headers = {
             'Content-Type': "application/json",
@@ -17,8 +29,8 @@ def _():
         # assert (response.text == "...")
 
     @it('routing:get_mission_card_data')
-    def test1():
-        url = "http://localhost:8080/get_mission_card_data"
+    def test2():
+        url = "http://127.0.0.1:8080/get_mission_card_data"
         payload = ""
         headers = {
             'Content-Type': "application/json",
