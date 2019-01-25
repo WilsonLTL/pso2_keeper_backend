0546b8d04f8b873a596b025b7c81530cf9bcdccc
Wilson lo <wilsonlo1997@gmail.com>
2019-01-25 15:29:52 +0800

    deploy EB

---
+++ gitlab-deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y -qq python python-dev python-pip ca-certificates
+pip install awsebcli --upgrade --ignore-installed
+
+mkdir ~/.aws/
+touch ~/.aws/credentials
+printf "[eb-cli]\naws_access_key_id = %s\naws_secret_access_key = %s\n" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+touch ~/.aws/config
+printf "[profile eb-cli]\nregion=us-west-2\noutput=json" >> ~/.aws/config
